e68bb204b2ad128ad4d121dace68e96e073eed8b
Brian Lehrer <nobut@pop-os.localdomain>
2024-10-22 01:41:45 -0700

    make it more like intellij

--- init.lua
+++ init.lua
@@ -165,8 +165,7 @@ vim.opt.scrolloff = 10
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader><f2>', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
+vim.keymap.set('n', '<leader><f2>', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfix list' })
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
