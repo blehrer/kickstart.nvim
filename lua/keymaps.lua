e78588c46c9a5b53609e09c5cc65b12993797a51
Lehrer, Brian <661570+blehrer@users.noreply.github.com>
2025-01-29 00:00:58 -0800

    migrate to kickstart-modular.nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,7 +6,7 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>x', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfi[x] list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -26,10 +26,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-w>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -44,5 +44,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+RenameFile = function()
+  local old_name = vim.fn.expand '%'
+  local new_name = vim.fn.input('New file name: ', vim.fn.expand '%', 'file')
+  if new_name ~= '' and new_name ~= old_name then
+    vim.fn.execute(':saveas ' + new_name)
+    vim.fn.execute(':silent !rm ' + old_name)
+  end
+end
+
+vim.keymap.set('n', '<leader>w', ':w<cr>', { desc = '[W]rite' })
+vim.keymap.set('n', '<leader>q', ':q<cr>', { desc = '[Q]uit' })
+vim.keymap.set('n', '<A-j>', 'a<cr><esc>k$', { desc = 'Split lines (opposite of `shift+j`)' })
+vim.keymap.set('n', '<C-j>', 'a<cr><esc>k$', { desc = 'Split lines (opposite of `shift+j`)' })
+vim.keymap.set('n', '<F18>', ':lua RenameFile()<cr>', { desc = 'Rename file' })
+vim.keymap.set('n', '<leader>l', ':Lazy<cr>', { desc = '[L]azy plugin manager' })
+vim.cmd.cabbrev('w!!', 'w !SUDO_ASKPASS="/usr/bin/pass $USER" sudo --askpass tee % > /dev/null')
+vim.keymap.set('n', '-', ':Oil<cr>', { desc = 'Edit directory with oil.nvim' })
 
 -- vim: ts=2 sts=2 sw=2 et
