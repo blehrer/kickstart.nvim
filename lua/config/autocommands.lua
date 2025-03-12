a34402842841f9ac19c8517db14758adbeffb141
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-03-11 23:08:58 -0700

    fixup! peek (markdown preview)

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -2,8 +2,10 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'markdown' },
   callback = function()
     vim.schedule(function()
-      vim.keymap.set('n', '<leader>p', ':TogglePeek<cr>', { desc = 'Toggle markdown preview (peek.nvim)' })
+      vim.keymap.set('n', '<LocalLeader>p', ':TogglePeek<cr>', {
+        desc = 'Toggle markdown preview (peek.nvim)',
+        buffer = vim.api.nvim_get_current_buf(),
+      })
     end)
   end,
 })
-
