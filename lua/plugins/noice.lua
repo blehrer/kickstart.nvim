00e23477e2537f0ff5082d20982ff002d119ba8f
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-03-11 22:12:55 -0700

    remove customizatinos to noice cmdline icons after changing font to Hasklug Nerd Font

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,9 +7,9 @@ return {
     cmdline = {
       ---@type table<string, CmdlineFormat>
       format = {
-        cmdline = { pattern = '^:', icon = '❱', lang = 'vim' },
-        search_down = { kind = 'search', pattern = '^/', icon = '  ', lang = 'regex' },
-        search_up = { kind = 'search', pattern = '^%?', icon = '  ', lang = 'regex' },
+        -- cmdline = { pattern = '^:', icon = '❱', lang = 'vim' },
+        -- search_down = { kind = 'search', pattern = '^/', icon = '  ', lang = 'regex' },
+        -- search_up = { kind = 'search', pattern = '^%?', icon = '  ', lang = 'regex' },
       },
     },
   },
