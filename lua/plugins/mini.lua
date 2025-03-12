dc8e37e61b536e3a2d04b07af4facd0a20ae872a
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-03-11 23:13:59 -0700

    get ministatusline to show when i am recording a macro

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -20,17 +20,58 @@ return { -- Collection of various small independent plugins/modules
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
     local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
+    statusline.setup {
+      use_icons = vim.g.have_nerd_font,
+      content = {
+        active = function()
+          -- Track the start of macro recording
+          vim.api.nvim_create_autocmd('RecordingEnter', {
+            pattern = '*',
+            callback = function()
+              vim.g.macro_recording = 'Recording @' .. vim.fn.reg_recording()
+              vim.cmd 'redrawstatus'
+            end,
+          })
+          -- Track the end of macro recording
+          vim.api.nvim_create_autocmd('RecordingLeave', {
+            pattern = '*',
+            callback = function()
+              vim.g.macro_recording = ''
+              vim.cmd 'redrawstatus'
+            end,
+          })
+          local check_macro_recording = function()
+            if vim.fn.reg_recording() ~= '' then
+              return 'Recording @' .. vim.fn.reg_recording()
+            else
+              return ''
+            end
+          end
 
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
+          local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+          local git = MiniStatusline.section_git { trunc_width = 40 }
+          local diff = MiniStatusline.section_diff { trunc_width = 75 }
+          local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+          local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+          local filename = MiniStatusline.section_filename { trunc_width = 140 }
+          local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+          local location = MiniStatusline.section_location { trunc_width = 200 }
+          local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+          local macro = check_macro_recording()
 
+          return MiniStatusline.combine_groups {
+            { hl = mode_hl, strings = { mode } },
+            { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics } },
+            '%<', -- Mark general truncate point
+            { hl = 'MiniStatuslineFilename', strings = { filename } },
+            '%=', -- End left alignment
+            { hl = 'MiniStatuslineFilename', strings = { macro } },
+            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo, lsp } },
+            { hl = mode_hl, strings = { search, '%2l:%-2v' } },
+          }
+        end,
+      },
+    }
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
