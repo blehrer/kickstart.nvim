b97c8117a725fc4792ecac0e0dae6ebc1bf6d62d
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-05-05 21:16:37 -0700

    ui plugins for symbols sidebar and todo comments

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,29 +1,8 @@
-require('lazy.types')
+require 'lazy.types'
 ---@type LazyPluginSpec[]
 return {
   {
     'folke/persistence.nvim',
-    dependencies = {
-      'folke/edgy.nvim',
-      'oskarrrrrrr/symbols.nvim',
-      'roobert/hoversplit.nvim',
-    },
-    opts = function()
-      local edgy = require('edgy')
-      local hoversplit = require('hoversplit')
-      edgy.setup({
-        ---@type Edgy.View.Opts[]
-        right = {
-          {
-            title = 'Lsp Symbols',
-            ft = 'snacks_picker_list',
-            filter = function(buf, win)
-              return vim.api.nvim_win_get_config(win).relative == ''
-            end,
-            pinned = true
-          }
-        }
-      })
-    end
-  }
+    opts = {},
+  },
 }
