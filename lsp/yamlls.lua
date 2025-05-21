4f0c4c4e94d13282f7473d99a34d1245bac3bc36
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-05-20 19:58:17 -0700

    bugfix for yamlls cfg

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -1,17 +1,20 @@
-local customizations = vim.deepcopy(require('lspconfig.configs.yamlls').default_config)
+---@type vim.lsp.ClientConfig
+return {
+  capabilities = {
+    textDocument = {
+      foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      },
+      documentLink = {
+        dynamicRegistration = true,
+      },
+    },
+  },
 
--- Have to add this for yamlls to understand that we support line folding
-customizations.capabilities = {
-  textDocument = {
-    foldingRange = {
-      dynamicRegistration = false,
-      lineFoldingOnly = true,
+  settings = {
+    yaml = {
+      schemas = require('schemastore').yaml.schemas(),
     },
   },
 }
-
-customizations.on_new_config = function(new_config)
-  vim.tbl_deep_extend('force', new_config.settings.yaml.schemas or {}, require('schemastore').yaml.schemas())
-end
-
-return customizations
