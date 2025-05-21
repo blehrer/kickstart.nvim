6af5f684db58c1d7f7a841b49d17060c3e00e769
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-05-20 22:30:56 -0700

    fix <leader>cl loggers for lua python and typescript

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,11 +1,25 @@
-vim.keymap.set('n', '<leader>dl', function()
+vim.keymap.set('n', '<leader>ds', function()
   require('osv').launch { port = 8086 }
 end, {
   noremap = true,
   buffer = 0,
-  desc = '[D]ebug: Launch neovim [l]ua debugger',
+  desc = '[D]ebug: Launch neovim lua debugg [s]erver',
 })
 
-local esc = vim.keycode '<Esc>'
 -- Log highlighted phase as a notification
-vim.fn.setreg('l', 'yo' .. 'vim.notify(("' .. esc .. 'pa: %s"):format(vim.inspect(' .. esc .. 'pa)))' .. esc)
+vim.keymap.set('v', '<leader>cl', function()
+  local esc = vim.keycode '<Esc>'
+  local macro = table.concat({
+    'yo',
+    'vim.notify(("',
+    esc,
+    'pa: %s"):format(vim.inspect(',
+    esc,
+    'pa)))',
+    esc,
+  }, '')
+
+  vim.fn.setreg('l', macro)
+  -- vim.notify(('setreg: %s'):format(vim.inspect(setreg)))
+  vim.cmd 'norm @l'
+end, { desc = '[C]ode: [L]og highlighted as a notification' })
--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,3 +1,17 @@
-local esc = vim.keycode '<Esc>'
--- Log highlighted phase
-vim.fn.setreg('l', 'yo' .. 'print(f' .. esc .. 'pa' .. ': {' .. esc .. 'pa' .. '})' .. esc)
+-- Log highlighted phase as a notification
+vim.keymap.set('v', '<leader>cl', function()
+  local esc = vim.keycode '<Esc>'
+  local macro = table.concat({
+    'yo',
+    'print(f',
+    esc,
+    'pa',
+    ': {',
+    esc,
+    'pa',
+    '})',
+    esc,
+  }, '')
+  vim.fn.setreg('l', macro)
+  vim.cmd 'norm @l'
+end, { desc = '[C]ode: [L]og highlighted as a notification' })
