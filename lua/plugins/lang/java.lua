f762f3fb75582f94e87631c7e23721d97da52119
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-05-22 03:08:26 -0700

    fix java plugin

--- lua/plugins/lang/java.lua
+++ lua/plugins/lang/java.lua
@@ -1,13 +1,45 @@
----@module 'lazy.types'
----@type LazyPluginSpec
 return {
   'nvim-java/nvim-java',
+  config = false,
   dependencies = {
-    'neovim/nvim-lspconfig',
+    {
+      'neovim/nvim-lspconfig',
+      opts = {
+        servers = {
+          -- Your JDTLS configuration goes here
+          jdtls = {
+            -- settings = {
+            --   java = {
+            --     configuration = {
+            --       runtimes = {
+            --         {
+            --           name = "JavaSE-23",
+            --           path = "/usr/local/sdkman/candidates/java/23-tem",
+            --         },
+            --       },
+            --     },
+            --   },
+            -- },
+          },
+        },
+        setup = {
+          jdtls = function()
+            -- Your nvim-java configuration goes here
+            require('java').setup {
+              -- root_markers = {
+              --   "settings.gradle",
+              --   "settings.gradle.kts",
+              --   "pom.xml",
+              --   "build.gradle",
+              --   "mvnw",
+              --   "gradlew",
+              --   "build.gradle",
+              --   "build.gradle.kts",
+              -- },
+            }
+          end,
+        },
+      },
+    },
   },
-  lazy = true,
-  cond = function()
-    local rp = require('lspconfig.util').root_pattern(require('java.config').root_markers)
-    return rp(vim.uv.cwd()) and true or false
-  end,
 }
