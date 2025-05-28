f46e5bb39508084eb5efbe755a02ed00b6bfa714
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-05-28 16:51:57 -0700

    C-Enter code actions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -157,6 +157,7 @@ return {
       -- Execute a code action, usually your cursor needs to be on top of an error
       -- or a suggestion from your LSP for this to activate.
       { '<leader>ca', vim.lsp.buf.code_action, desc = '[C]ode [A]ction', { 'n', 'x' } },
+      { '<C-Enter>', vim.lsp.buf.code_action, desc = '[C]ode [A]ction', { 'n', 'x' } },
 
       -- WARN: This is not Goto Definition, this is Goto Declaration.
       --  For example, in C this would take you to the header.
