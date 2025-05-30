47222715eee3cbe7f9f1a5b84114b07452889b26
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-05-30 14:58:29 -0700

    scissor snips!

--- lua/plugins/scissors.lua
+++ lua/plugins/scissors.lua
@@ -9,4 +9,30 @@ return {
     snippetDir = vim.fs.joinpath(vim.fn.stdpath 'config', 'snippets', 'vscode'),
   },
   dependencies = { 'folke/snacks.nvim' },
+  ---@type LazyKeysSpec[]
+  keys = {
+    {
+      '<leader>cse',
+      function()
+        require('scissors').editSnippet()
+      end,
+      desc = '[C]ode: edit snippet',
+    },
+    {
+      '<leader>csa',
+      function()
+        require('scissors').addNewSnippet()
+      end,
+      desc = '[C]ode: add snippet',
+      mode = { 'n', 'x' },
+    },
+    {
+      '<leader>csa',
+      function()
+        require('scissors').addNewSnippet "'<,'>"
+      end,
+      desc = '[C]ode: add snippet from selection',
+      mode = { 'v' },
+    },
+  },
 }
