79cd197a4e16b7565c7b3728e145ee21b6f6d58e
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-06-02 12:03:34 -0700

    chezmoi condition

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,3 +1,7 @@
 function Inspect(obj)
   vim.notify(vim.inspect(obj))
 end
+
+function Has_chezmoi()
+  return vim.system({ 'command', '-v', 'chezmoi' }):wait().code == 0
+end
--- lua/plugins/lang/chezmoi.lua
+++ lua/plugins/lang/chezmoi.lua
@@ -1,7 +1,11 @@
+---@module 'config.functions'
+---@module 'lazy.types'
+---@type LazyPluginSpec[]
 return {
   {
     -- highlighting for chezmoi files template files
     'alker0/chezmoi.vim',
+    cond = HasChezmoi,
     init = function()
       vim.g['chezmoi#use_tmp_buffer'] = 1
       vim.g['chezmoi#source_dir_path'] = os.getenv 'HOME' .. '/.local/share/chezmoi'
@@ -9,6 +13,7 @@ return {
   },
   {
     'andre-kotake/nvim-chezmoi',
+    cond = HasChezmoi,
     dependencies = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-telescope/telescope.nvim' },
