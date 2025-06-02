450b89b37d60510c2fcc647f96fd5a8c5190667a
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-06-02 12:09:48 -0700

    java mise fixes

--- lua/plugins/lang/java.lua
+++ lua/plugins/lang/java.lua
@@ -1,43 +1,51 @@
 local log_level = vim.g.plugin_log_level
 local log = require('logger'):new { log_level = log_level, prefix = ('%s'):format(debug.getinfo(1, 'S').short_src), echo_messages = false }
+
 ---@param tool string
 ---@param version string | number
 ---@param default 'default'?
 local function mise_where(tool, version, default)
-  local name = tool .. '@' .. version
-  local success, result = pcall(function()
-    return vim.system({ 'mise', 'where', name }, { text = true }):wait()
-  end)
-  if not success or result.code ~= 0 then
-    log:error('`mise where' .. name .. '` failed')
-    local term, code = os.execute 'command -v mise'
-    if term == 'signal' then
-      log:error('`command -v mise` was interrupted by signal', result.signal)
-    elseif code ~= 0 then
-      log:error '`mise` is not a known command'
-    end
-  else
-    log:debug('result:', result)
-    local path = success and result.stdout:gsub('%s', '') or nil
-    local is_default = default and true or false
-    if name and path then
-      return {
-        name = name,
-        path = path,
-        default = is_default,
-      }
+  if vim.system({ 'command', '-v', 'mise' }, { text = true }):wait().code == 0 then
+    local name = tool .. '@' .. version
+    local success, result = pcall(function()
+      return vim.system({ 'mise', 'where', name }, { text = true }):wait()
+    end)
+    if not success or result.code ~= 0 then
+      log:error('`mise where ' .. name .. '` failed')
+      local term, code = os.execute 'command -v mise'
+      if term == 'signal' then
+        log:error('`command -v mise` was interrupted by signal', result.signal)
+      elseif code ~= 0 then
+        log:error '`mise` is not a known command'
+      end
+    else
+      log:debug('result:', result)
+      local path = success and result.stdout:gsub('%s', '') or nil
+      local is_default = default and true or false
+      if name and path then
+        return {
+          name = name,
+          path = path,
+          default = is_default,
+        }
+      end
     end
   end
 end
 
+---@module 'config.functions'
 ---@module 'lazy.types'
 ---@type LazyPluginSpec
 return {
   'nvim-java/nvim-java',
   lazy = true,
+  cond = HasChezmoi,
   dependencies = {
     {
       'neovim/nvim-lspconfig',
+      cond = function()
+        return vim.system({ 'command', '-v', 'mise' }, { text = true }):wait().code == 0
+      end,
       opts = {
         servers = {
           jdtls = {
