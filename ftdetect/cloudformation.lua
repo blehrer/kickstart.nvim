3e2a7ade839b43d547ce8e6548fe656a2eef50b7
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-06-03 15:40:18 -0700

    fixup! cloudformation/yamlls tweaks

--- ftdetect/cloudformation.lua
+++ ftdetect/cloudformation.lua
@@ -32,17 +32,19 @@ vim.filetype.add {
     ['.*'] = {
       priority = math.huge,
       function(_, bufnr)
-        local line1 = vim.filetype.getlines(bufnr, 1)
-        local line2 = vim.filetype.getlines(bufnr, 2)
-        if vim.filetype.matchregex(line1, [[^AWSTemplateFormatVersion]]) or vim.filetype.matchregex(line1, [[AWS::Serverless-2016-10-31]]) then
-          return 'yaml.cloudformation'
-        elseif
-          vim.filetype.matchregex(line1, [[["']AWSTemplateFormatVersion]])
-          or vim.filetype.matchregex(line2, [[["']AWSTemplateFormatVersion]])
-          or vim.filetype.matchregex(line1, [[AWS::Serverless-2016-10-31]])
-          or vim.filetype.matchregex(line2, [[AWS::Serverless-2016-10-31]])
-        then
-          return 'json.cloudformation'
+        if not (vim.bo[bufnr].filetype:gmatch 'picker') then
+          local line1 = vim.filetype.getlines(bufnr, 1)
+          local line2 = vim.filetype.getlines(bufnr, 2)
+          if vim.filetype.matchregex(line1, [[^AWSTemplateFormatVersion]]) or vim.filetype.matchregex(line1, [[AWS::Serverless-2016-10-31]]) then
+            return 'yaml.cloudformation'
+          elseif
+            vim.filetype.matchregex(line1, [[["']AWSTemplateFormatVersion]])
+            or vim.filetype.matchregex(line2, [[["']AWSTemplateFormatVersion]])
+            or vim.filetype.matchregex(line1, [[AWS::Serverless-2016-10-31]])
+            or vim.filetype.matchregex(line2, [[AWS::Serverless-2016-10-31]])
+          then
+            return 'json.cloudformation'
+          end
         end
       end,
     },
