d3439124f2c7d98d1cf5fe468e7706a49ad0d63e
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-06-17 21:09:30 -0700

    fix copypaste error

--- lua/plugins/refjump.lua
+++ lua/plugins/refjump.lua
@@ -17,7 +17,6 @@ return {
         build = ':TSUpdate',
         config = function()
           require('nvim-treesitter.configs').setup {
-            ensure_installed = 'all',
             textobjects = {
               move = {
                 enable = true,
