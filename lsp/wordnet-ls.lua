eba0a6b8703c997329660b2ac8691fb74e07cabe
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-06-22 16:10:45 -0700

    fix wordnet path

--- lsp/wordnet-ls.lua
+++ lsp/wordnet-ls.lua
@@ -1,10 +1,11 @@
+local brew_prefix = os.getenv 'HOMEBREW_PREFIX' or '/opt/homebrew'
 ---@type vim.lsp.ClientConfig
 return {
   name = 'wordnet-ls',
   cmd = { vim.fs.joinpath(os.getenv 'HOME', '.cargo', 'bin', 'wordnet-ls'), '--stdio' },
   filetypes = { 'text', 'markdown' },
   init_options = {
-    wordnet = vim.fs.joinpath(os.getenv 'HOMEBREW_PREFIX', 'Cellar', 'wordnet', '3.1_2', 'dict'),
+    wordnet = vim.fs.joinpath(brew_prefix, 'Cellar', 'wordnet', '3.1_2', 'dict'),
     enable_completion = false,
     enable_hover = true,
     enable_code_actions = true,
