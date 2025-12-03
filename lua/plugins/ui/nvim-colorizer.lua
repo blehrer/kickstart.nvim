c8e6b6df5e28e61061138c4659262b65e8e7b385
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-12-03 14:58:42 -0800

    colorizer conf

--- lua/plugins/ui/nvim-colorizer.lua
+++ lua/plugins/ui/nvim-colorizer.lua
@@ -1,13 +1,18 @@
 return {
-  {
-    'Makaze/AnsiEsc',
-    opts = {},
-  },
+  -- {
+  --   'Makaze/AnsiEsc',
+  --   opts = {},
+  -- },
   {
     'catgoose/nvim-colorizer.lua',
     event = 'BufReadPre',
     enabled = true,
     opts = { -- set to setup table
+      user_default_options = {
+        names = false,
+        tailwind = true,
+        xterm = true,
+      },
     },
   },
 }
