b021004014a7c4a2e5384ea71b814eeaaf561bfa
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-12-31 14:03:08 -0800

    prettier md formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -58,6 +58,7 @@ return {
         sh = { 'beautysh' },
         bash = { 'beautysh' },
         zsh = { 'beautysh' },
+        markdown = { 'prettier' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
