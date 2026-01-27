f50c792941828e6f11cd4e826dd5bc52f4fe319e
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2026-01-26 19:00:27 -0800

    golang null_case

--- lua/plugins/lang/golang.lua
+++ lua/plugins/lang/golang.lua
@@ -1,37 +1,44 @@
----@module 'lazy.types'
----@type LazyPluginSpec[]
-return {
-  {
-    'ray-x/go.nvim',
-    dependencies = { -- optional packages
-      'ray-x/guihua.lua',
-      'neovim/nvim-lspconfig',
-      'nvim-treesitter/nvim-treesitter',
+local function null_case()
+  return #vim.fn.exepath 'go' == 0
+end
+if null_case() then
+  return {}
+else
+  ---@module 'lazy.types'
+  ---@type LazyPluginSpec[]
+  return {
+    {
+      'ray-x/go.nvim',
+      dependencies = { -- optional packages
+        'ray-x/guihua.lua',
+        'neovim/nvim-lspconfig',
+        'nvim-treesitter/nvim-treesitter',
+      },
+      opts = {
+        -- lsp_keymaps = false,
+        -- other options
+      },
+      -- config = function(_, opts)
+      --   require('go').setup(opts)
+      --   local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+      --   vim.api.nvim_create_autocmd('BufWritePre', {
+      --     pattern = '*.go',
+      --     callback = function()
+      --       require('go.format').goimports()
+      --     end,
+      --     group = format_sync_grp,
+      --   })
+      -- end,
+      event = { 'CmdlineEnter' },
+      ft = { 'go', 'gomod' },
+      build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
-    opts = {
-      -- lsp_keymaps = false,
-      -- other options
+    {
+      'ngalaiko/tree-sitter-go-template',
+      opts = {},
+      config = function()
+        local m = require 'go.template'
+      end,
     },
-    -- config = function(_, opts)
-    --   require('go').setup(opts)
-    --   local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
-    --   vim.api.nvim_create_autocmd('BufWritePre', {
-    --     pattern = '*.go',
-    --     callback = function()
-    --       require('go.format').goimports()
-    --     end,
-    --     group = format_sync_grp,
-    --   })
-    -- end,
-    event = { 'CmdlineEnter' },
-    ft = { 'go', 'gomod' },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
-  {
-    'ngalaiko/tree-sitter-go-template',
-    opts = {},
-    config = function()
-      local m = require 'go.template'
-    end,
-  },
-}
+  }
+end
