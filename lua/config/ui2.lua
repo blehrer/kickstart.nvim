35541cbaf322684d0c1e786ec3c670703485166d
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2026-08-26 13:26:19 -0700

    ui(tiny-cmdline): double border on centered cmdline
    Set explicit double border on tiny-cmdline; normalize ui2 quote style.

--- lua/config/ui2.lua
+++ lua/config/ui2.lua
@@ -1,11 +1,11 @@
 vim.o.cmdheight = 0
-vim.o.winborder = 'rounded'
+vim.o.winborder = "rounded"
 
-require('vim._core.ui2').enable({
-  msg = {
-    targets = 'cmd',
-    cmd = { height = 0.5 },
-    msg = { height = 0.5, timeout = 4000 },
-    pager = { height = 1 },
-  },
+require("vim._core.ui2").enable({
+	msg = {
+		targets = "cmd",
+		cmd = { height = 0.5 },
+		msg = { height = 0.5, timeout = 4000 },
+		pager = { height = 1 },
+	},
 })
--- plugin/05-tiny-cmdline.lua
+++ plugin/05-tiny-cmdline.lua
@@ -1,9 +1,11 @@
-if vim.g.vscode then return end
+if vim.g.vscode then
+	return
+end
 
 -- ponytail: centers ui2 cmdline; requires config.ui2 loaded first from init.lua
-vim.pack.add({ 'https://github.com/rachartier/tiny-cmdline.nvim' })
+vim.pack.add({ "https://github.com/rachartier/tiny-cmdline.nvim" })
 
-require('tiny-cmdline').setup({
-  border = nil, -- inherit vim.o.winborder
-  native_types = {}, -- ponytail: try centered search; restore { '/', '?' } if it feels wrong
+require("tiny-cmdline").setup({
+	border = "double", -- inherit vim.o.winborder
+	native_types = {}, -- ponytail: try centered search; restore { '/', '?' } if it feels wrong
 })
